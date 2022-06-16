9df848851dfdf073b4c5518366519390bdbd97ab
Viktor Holovin <holovin@mail.com>
2022-06-16 20:45:46 +0200

    Create dynamic objects

--- main.py
+++ main.py
@@ -33,32 +33,28 @@ app.add_middleware(
 articles = os.listdir(path_to_camera)
 photos = []
 videos = []
-i = 0
-
-for article in articles:
-    if article.endswith(".jpg") or article.endswith(".png"):
-        photos.append(
-            {
-                "id": i,
-                "name": article,
-                "url_to_photo": url + "static/" + article,
-                "path_to_photo": path_to_camera + article,
-                "url_to_thumbnail": url + "static/" + "thumbnails/" + article + ".thumbnail",
-            }
-        )
-        i = i+1
-
-for article in articles:
-    if article.endswith(".mp4"):
-        videos.append({"id": i, "path_to_video": url+"static/"+article})
-        i = i+1
 
 @app.get("/")
 def read_hello():
     return {"hello": "world"}
 
-@app.get("/photos")
-def read_photos():
+@app.get("/scan-photos")
+def scan_photos():
+    i = 0
+    for article in articles:
+        if article.endswith(".jpg") or article.endswith(".png"):
+            photos.append(
+                {
+                    "id": i,
+                    "url_api": "photos/" + str(i),
+                    "name": article,
+                    "url_to_photo": url + "static/" + article,
+                    "path_to_photo": path_to_camera + article,
+                    "url_to_thumbnail": url + "static/" + "thumbnails/" + article + ".thumbnail",
+                }
+            )
+            i = i + 1
+
     size = 380, 640
     if not os.path.isdir(path_to_camera + "thumbnails"):
         os.mkdir(path_to_camera + "thumbnails")
@@ -68,7 +64,29 @@ def read_photos():
             img.save(path_to_camera + "thumbnails/" + photo["name"] + ".thumbnail", "JPEG")
     thumbnails = os.listdir(path_to_camera + "thumbnails")
     thumbnails.sort(reverse=True)
-    return photos
+    return {"scan_photos": True}
+
+@app.get("/photos")
+def read_photos():
+    photos_api = []
+    for photo in photos:
+        photos_api.append(
+            {
+                "id": photo["id"],
+                "url": url + photo["url_api"],
+            }
+        )
+    return photos_api
+
+@app.get("/photos/{photo_id}")
+async def read_photo(photo_id):
+    return {
+        "id": photos[int(photo_id)]["id"],
+        "name": photos[int(photo_id)]["name"],
+        "url_to_photo": photos[int(photo_id)]["url_to_photo"],
+        "path_to_photo": photos[int(photo_id)]["path_to_photo"],
+        "url_to_thumbnail": photos[int(photo_id)]["url_to_thumbnail"],
+    }
 
 @app.get("/videos")
 def read_videos():
